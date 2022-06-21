49fe8e7627c4ee172442206759a9cf877fe3b8dd
Ramis <akhmetovramis1@gmail.com>
2022-06-21 13:14:27 +0600

    Добавьте файлы проекта.

---
+++ IClinic.Beckend.Prometheus/Controllers/ValuesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Prometheus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace IClinic.Beckend.Prometheus.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        Counter counter = Metrics.CreateCounter("my_counter", "Metrict counter");
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public Counter Get()
+        {
+            while (true)
+            {
+                counter.Inc();
+                return counter;
+            }
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+    }
+}
